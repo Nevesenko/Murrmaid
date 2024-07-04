edbf662bd9f2eada88a06e4a44707af1c671bc3b
kftoen <nnnevesenko@gmail.com>
2024-07-04 07:05:54 +0300

    Refactor patch block function

--- src/business/business.py
+++ src/business/business.py
@@ -18,19 +18,14 @@ def update_mermaid_graph(id, conn = None):
     cur = excluding_categories.exclude_title_fields(cur, 'properties.Name.title' )
     nodes = draw_mermaid.draw_nodes(cur)
     #####
-
     conns = nback.parse_data(conn)
     conns = cleaning_data.get_info(conns)
-    conns = conns[['properties.Child.relation','properties.Parent.relation']]
-    conns = conns.map(lambda x : x[0]['id'])
+    relations = ['properties.Child.relation','properties.Parent.relation'] # подумать об алгоритме формирования этой переменной
+    conns = excluding_categories.exclude_category_field(conns, relations)
     conns = conns.rename(columns={'properties.Child.relation' : 'Child', 'properties.Parent.relation': 'Parent'})
     connections = draw_mermaid.draw_graph(conns)
-    print(connections)
     final_string = draw_mermaid.form_the_graph_schema(nodes, connections)
-    print(final_string)
     body = {  "code": {"rich_text": [{"type": "text","text": {"content": final_string}}],"language": "mermaid"}}
-    o = requests.patch('https://api.notion.com/v1/blocks/3214708db6884b5b94d11e5aef0c78b7',
-                   headers=nback.headers,
-                       json = body)
-    print(o)
+    nback.patch_block('3214708db6884b5b94d11e5aef0c78b7', body)
+
 update_mermaid_graph(id_text, id_conn)
